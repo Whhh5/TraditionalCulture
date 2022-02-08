1d91cfa74740dda6cb1f79108f7b0c8235540385
王佳浩 <2206222540@qq.com>
2022-02-08 15:49:08 +0800

    init

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : SingletonPanttern<Test>
+{
+    public int number = 5;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha6))
+        {
+
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha7))
+        {
+
+        }
+    }
+}
